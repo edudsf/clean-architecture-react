ad85febc668b22bcccb9ac88b9ff311f2d059dab
EDUDSF <eduardo.sf.1992@gmail.com>
2021-05-09 15:59:36 -0300

    chore: config webpack

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts, tsx}'
+    '<rootDir>/src/**/*.{ts, tsx}',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
@@ -10,6 +11,7 @@ module.exports = {
     '.+\\.ts?$': 'ts-jest'
   },
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts','.tsx','.js'],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
