42bd1ac9c30fb8858089b5fe7fea9d7a7c395ed8
EDUDSF <eduardo.sf.1992@gmail.com>
2021-05-11 22:35:11 -0300

    test: ensure Login start with inputs required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,7 +7,14 @@ describe('Login Component', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    /** Test buttton submit */
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+    /** Test input email */
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    /** Test input password */
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
   })
 })
