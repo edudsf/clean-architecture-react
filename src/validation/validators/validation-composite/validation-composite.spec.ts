5573e5046551b9078ef7dcf101c78f38ae7e54ab
EDUDSF <eduardo.sf.1992@gmail.com>
2021-05-14 16:40:04 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,28 @@
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+  const sut = new ValidationComposite(fieldValidationSpy)
+  return {
+    sut,
+    fieldValidationSpy
+  }
+}
+
 describe('ValidationComposite', () => {
   test('Should return error if away validation fails ', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_error_message')
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2
-    ])
+    const { sut, fieldValidationSpy } = makeSut()
+    fieldValidationSpy[0].error = new Error('any_error_message')
+    fieldValidationSpy[1].error = new Error('any_error_message')
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('any_error_message')
   })
